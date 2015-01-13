f0eeb47452a05549d66a288ad906f4792e2979b0
ratajx <ratajx93@gmail.com>
2015-01-13 07:58:40 +0100

    Dodanie projektu

---
+++ tester/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AccessibleName = "";
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(483, 295);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(475, 269);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Wczytywanie pliku";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ścieżka:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Długość:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(463, 196);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wczytaj plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.checkBox2);
+            this.tabPage2.Controls.Add(this.checkBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(475, 269);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Test statyczny";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Location = new System.Drawing.Point(25, 101);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(435, 150);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Uruchom test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(25, 53);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(93, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Test par bitów";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(143, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Test pojedyńczych bitów";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox3);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.checkBox4);
+            this.tabPage3.Controls.Add(this.checkBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(475, 269);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Test statyczny wg FIPS";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AllowDrop = true;
+            this.textBox3.Location = new System.Drawing.Point(25, 101);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(435, 150);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(200, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Uruchom test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(25, 53);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(96, 17);
+            this.checkBox4.TabIndex = 1;
+            this.checkBox4.Text = "Test pokerowy";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(25, 20);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(103, 17);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "Test długiej serii";
+            this.checkBox3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 319);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "TESTER";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ tester/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace tester
+{
+    public partial class Form1 : Form
+    {
+        int[] tab;
+        int lenght;
+    
+        double[] pojedyncze()
+        {
+            int n0=0,n1=0;
+            double[] t=new double[3];
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] == 0)
+                    n0++;
+                else
+                    n1++;
+            }
+
+            double res;
+            res = (double)((n0 - n1) * (n0 - n1)) /(double) tab.Length;
+
+
+            t[0] = n0;
+            t[1] = n1;
+            t[2] = res;
+            return t;
+        }
+
+        void pary()
+        {
+            double[] t = pojedyncze();
+            double n0 = t[0], n1 = t[1];
+
+            int[,] tabk = new int[,] 
+            {{ 00, 0 }, { 01, 0 }, { 10, 0 }, { 11, 0 },};
+
+            int number = 0;
+
+            for (int i = 0; tab.Length - i > 1; i++)
+            {
+                number = tab[i] * 10 + tab[i + 1];
+                for (int j = 0; j < 4; j++)
+                    if (number == tabk[j, 0])
+                    {
+                        tabk[j, 1]++;
+                        break;
+                    }
+            }
+            double suma=0;
+
+            for(int i=0;i<4;i++)
+                suma+=tabk[i,1]*tabk[i,1];
+
+            double a = (double)4 / (double)(tab.Length - 1);
+            double b = (double)2 / (double)tab.Length;
+           
+            double res;
+            res = (a* suma) - (b * (n0 * n0 + n1 * n1)) + 1;
+           
+            textBox2.Text += checkBox2.Text + " S2 równe " + Math.Round(res, 4) + Environment.NewLine;
+
+        }
+
+        void dlugie()
+        {
+            int max = 0;
+
+            int[] v = new int[2];
+            v[0] = 0;
+            v[1] = 0;
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] == 0 && i == 0)
+                    v[0]++;
+                if (tab[i] == 1 && i == 0)
+                    v[1]++;
+
+                if (tab[i] == 0 && i > 0)
+                    if (tab[i - 1] == 0)
+                        v[0]++;
+                    else
+                    {
+                        if (v[1] > max)
+                            max = v[1];
+                        v[0] = 1;
+                        v[1] = 0;
+                    }
+
+                if (tab[i] == 1 && i > 0)
+                    if (tab[i - 1] == 1)
+                        v[1]++;
+                    else
+                    {
+                        if (v[0] > max)
+                            max = v[0];
+                        v[0] = 0;
+                        v[1] = 1;
+                    }
+             }
+            if (max > 25)
+                textBox3.Text += checkBox3.Text + " niezaliczony najdłuższa seria równa " + max + Environment.NewLine; 
+            else
+                textBox3.Text += checkBox3.Text + " zaliczony najdłuższa seria równa " + max + Environment.NewLine;
+        }
+
+        void poker()
+        {
+            int[,] tabk = new int[,] { 
+            { 0000, 0 }, { 0001, 0 }, { 0010, 0 }, { 0011, 0 },
+            { 0100, 0 }, { 0101, 0 }, { 0110, 0 }, { 0111, 0 }, 
+            { 1000, 0 }, { 1001, 0 }, { 1010, 0 }, { 1011, 0 }, 
+            { 1100, 0 }, { 1101, 0 }, { 1110, 0 }, { 1111, 0 },};
+            int number=0;
+            
+            for(int i=0;tab.Length-i>3;i+=4)
+            {
+                number = tab[i] * 1000 + tab[i + 1] * 100 + tab[i + 2] * 10 + tab[i + 3];
+                for(int j=0;j<16;j++)
+                    if (number == tabk[j,0])
+                    {
+                        tabk[j, 1]++;
+                        break;
+                    }
+            }
+
+            double temp,si=0;
+
+            for (int i = 0; i < 16; i++)
+            {
+                temp = (double)tabk[i, 1] * (double)tabk[i, 1];
+                si += temp;
+            }
+
+            double res;
+            res= (double)16 / ((double)tab.Length / 4) * si - 5000;
+
+            if (res > 2.16 && res < 46.17)
+                textBox3.Text += checkBox4.Text + " zaliczony X=" +Math.Round(res,4) + Environment.NewLine;
+            else
+                textBox3.Text += checkBox4.Text + " niezaliczony X=" + Math.Round(res, 4) + Environment.NewLine;
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          /* OpenFileDialog okienko = new OpenFileDialog();
+            okienko.Filter = "Pliki textowe (txt)|*.txt";
+            if (okienko.ShowDialog() == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                string path = okienko.FileName;
+                StreamReader sr = new StreamReader(path, Encoding.Default);
+                string s,txt="";
+                textBox1.Clear();
+                do
+                {
+                    s = sr.ReadLine();
+                    sb.AppendLine(s);
+                    txt+=s;
+                 } while (s != null);
+                textBox1.Text = textBox1.Text + txt;
+                label1.Text = txt.Length.ToString();
+                label2.Text = path.ToString();
+                sr.Close();*/
+
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            String txt = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            txt = File.ReadAllText(openFileDialog1.FileName);
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd. Nie można odczytać wskazenego pliku! \n " + ex.Message);
+                }
+
+               textBox1.AppendText(txt);
+
+                label1.Text = txt.Length.ToString();
+                label2.Text = openFileDialog1.FileName;
+
+                char[] tabb;
+                tabb = txt.ToCharArray();
+                int i=0;
+                tab = new int[tabb.Length];
+                foreach (char a in tabb)
+                {
+                    tab[i] = (int)a-48;
+                    i++;
+                }
+                lenght = tabb.Length;
+            }
+            else MessageBox.Show("Nie wczytano  pliku.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox2.Text += checkBox1.Text + " S1 równe " + Math.Round(pojedyncze()[2], 4) + Environment.NewLine;
+            }
+
+            if (checkBox2.Checked == true)
+            {
+                pary();
+            }
+        }
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lenght >= 20000)
+            {
+                if (checkBox3.Checked == true)
+                {
+                    dlugie();
+                }
+
+                if (checkBox4.Checked == true)
+                {
+                    poker();
+                }
+            }
+            else
+                MessageBox.Show("Ciąg jest za krótki, minimalna długość to 20000");
+        }
+
+      
+
+    
+    }
+}
